8aa63f8dc7bce39d1d06b199571df69ed1bfd5b6
Alexandre <alex.henri.2008@gmail.com>
2026-05-15 18:09:34 -0300

    Ex 01 e 02

--- Exercícios de aprendizado.py
+++ Exercícios de aprendizado.py
@@ -37,7 +37,7 @@ status = "✅" if tarefa["pronta"] else " "
 print(f"[{status}] {tarefa['descricao']}")
 
 
-#Ex:
+#Ex1:
 alunos ={}
 alunos['nome'] = str(input('nome: '))
 alunos['média'] = float(input(f'Média de {alunos["nome"]}'))
@@ -51,5 +51,34 @@ elif  5 <= alunos['média']  < 7:
 
 elif alunos['média' ] < 5:
     alunos['situação'] = 'Reprovado'
-print(alunos)
+
+print('-=' * 20)
+for chave, valor in alunos.items():
+    print(f'{chave} é igual a {valor}')
+
+#Ex2:
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogo = {'jogador 1': randint(1, 6), 
+        'jogador 2': randint(1, 6),
+        'jogador 3': randint(1, 6),
+        'jogador 4': randint(1, 6)}
+
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse= True)
+
+for j, d in jogo.items():
+    print(f'o {j} tirou {d}')
+    sleep(2)
+print('>--<'*20)
+for i, v in enumerate(ranking):
+    print(f'O {i+1} lugar é do {v[0]} com {v[1]}')
+    sleep(1)
+    
+
+
+
+
+
 #listas de dicionários
